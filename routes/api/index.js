98a5e081a93348fb0de20d14217bb3d61e6bf483
RichardVargas1 <richard.vargas95@gmail.com>
2019-01-30 21:04:01 -0600

    Included index.js in the routes/api directory

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,17 @@
-// const path = require("path");
-// const router = require("express".Router());
-// const speechRoutes = require("./speech");
-// const userAuthRoutes = require("./userAuth");
+const path = require("path");
+const router = require("express").Router();
+const speechRoutes = require("./speech");
+const userAuthRoutes = require("./userAuth");
+
+// Speech routes
+router.use("/speech", speechRoutes);
+
+// User Authentication Routes
+router.use("/userAuth", userAuthRoutes);
+
+// For anything else, render the html page
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
+module.exports = router;
