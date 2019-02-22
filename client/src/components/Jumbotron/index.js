c60ab417bf6d3cae7adbe3815328abc3f9da8908
RichardVargas1 <richard.vargas95@gmail.com>
2019-02-21 21:20:53 -0600

    Fixed Jumbo Typo

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -10,7 +10,7 @@ class Jumbotron extends Component {
           <Col size="md-12">
             <div id="jumbotron">
               <h1 className="text-center">
-                <strong>Welcome to Spech Share</strong>
+                <strong>Welcome to Speech Share</strong>
               </h1>
               <h2 className="text-center">User's will be able to create a profile. From there, they will be able to view speeches from a multitude of websites. Be it famous speeches made in a political, sports or film setting. From there, they will have the option of being able to post these speeches on their profile, or share them with other people, if they so wish. These speeches will be in text format, as well as video format: linked together.</h2>
             </div>
